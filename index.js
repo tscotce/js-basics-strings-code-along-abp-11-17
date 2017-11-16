b5025487a806964a85923cccb5e67fbdf0e92acb
tscotce <tscotce@gmail.com>
2017-11-16 04:31:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 const currentUser = 'Tatyana Scotce';
 const welcomeMessage = 'Welcome to Flatbook, ' + currentUser +'!';
-const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + currentUser + '!';
