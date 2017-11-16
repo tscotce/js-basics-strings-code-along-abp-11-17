e47538770ed42da8907ff1df4727096d00f77b54
tscotce <tscotce@gmail.com>
2017-11-16 04:37:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const currentUser = 'Tatyana Scotce';
-const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!'; //* + currentUser +'!';
-const excitedWelcomeMessage = welcomeMessage.toUpperCase();//*'WELCOME TO FLATBOOK, ' + upperCaseCurrentUser + '!';
+const welcomeMessage = 'Welcome to Flatbook, + currentUser +'!';
+const excitedWelcomeMessage = 'WELCOME TO FLATBOOK, ' + upperCaseCurrentUser + '!';
 const upperCaseCurrentUser = currentUser.toUpperCase ();
+const welcomeMessage = 'Welcome to Flatbook, ${currentUser}!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
